a35db5b2dc8435dcb04963be836c19d421c737a0
Clemente Zarraga <mcsarmend@gmail.com>
2023-05-12 11:01:29 -0600

    Inicio del proyecto

---
+++ resources/views/etiquetado/mambu/blao.blade.php
@@ -0,0 +1,27 @@
+@extends('adminlte::page')
+
+@section('title', 'Configuraciones')
+
+@section('content_header')
+    <h1>ENTRADA 2</h1>
+@stop
+
+@section('content')
+   <div class="card">
+    <div class="card-header">
+        <h1 class="card-title">Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque facere iusto maiores ipsum soluta veritatis, dolore unde necessitatibus nesciunt ipsam, reprehenderit, minima quae totam. Asperiores deleniti rerum praesentium iusto ipsum?</h1>
+    </div>
+    <div class="card-body">
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil laboriosam temporibus architecto, cumque accusamus asperiores in iusto dignissimos sequi. Odio alias commodi dicta repellat rerum quisquam! Ab aut nemo accusamus!</p>
+    </div>
+
+   </div>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
